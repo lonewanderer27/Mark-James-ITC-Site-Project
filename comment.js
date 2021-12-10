3504f75e99c517195a1efc631dfbee17abbb9a88
lonewanderer27 <adrianejames27@gmail.com>
2021-12-11 01:27:51 +0800

    first commit

---
+++ comment.js
@@ -0,0 +1,44 @@
+var ref = new Firebase("https://themarkdatabase-default-rtdb.firebaseio.com/");
+
+function timeStamp() {
+  var now = new Date();
+  var date = [now.getMonth() + 1, now.getDate(), now.getFullYear()];
+  var time = [now.getHours(), now.getMinutes()];
+  var suffix = (time[0] < 12) ? "AM" : "PM";
+  time[0] = (time[0] < 12) ? time[0] : time[0] - 12;
+
+  for (var i = 1; i < 3; i++) {
+    if (time[i] < 10) {
+      time[i] = "0" + time[i];
+    }
+  }
+
+  return date.join("/") + ", " + time.join(":") + " " + suffix;
+}
+
+function postComment() {
+  var name = document.getElementById("name").value,
+      comment = document.getElementById("comment").value,
+      time = timeStamp();
+
+  if (name && comment) {
+    ref.push({
+      name: name,
+      comment: comment,
+      time: time
+    });
+  }
+
+  document.getElementById("name").value = '';
+  document.getElementById("comment").value = '';
+}
+
+ref.on("child_added", function(snapshot) {
+  var comment = snapshot.val();
+  addComment(comment.name, comment.comment, comment.time);
+});
+
+function addComment(name, comment, timeStamp) {
+  var comments = document.getElementById("comments");
+  comments.innerHTML = "<hr><h4>" + name + "<span>" + timeStamp + "</span></h4><p>" + comment + "</p>" + comments.innerHTML;
+}
